2cb36920959cdc9cdaf36e6c9a780f18d8001946
Pawel Tarsala (PLT) <PLT@kmd.dk>
2018-12-24 03:49:53 +0100

    Game logic ended

--- Kulki/src/app/board/board.component.ts
+++ Kulki/src/app/board/board.component.ts
@@ -14,8 +14,11 @@ export class BoardComponent implements OnInit {
   private listOfFreePlaces: Array<Position>;
   private isClicked = false;
   private clickedKulka: BoardElement;
+  private futureKulkas: Array<KulkaColors>;
+  private isGameOver = false;
   public board: BoardElement[][];
   constructor() {
+    this.initFutureKulkas();
     this.listOfFreePlaces = new Array<Position>();
     this.board = [];
     for (let i = 0; i < Resources.NUMBER_OF_ROWS; i++) {
@@ -26,12 +29,19 @@ export class BoardComponent implements OnInit {
       }
     }
     this.addRandomKulkasOnBoard(Resources.NUMBER_OF_PER_TOUR);
+    this.getFutureKulkas();
   }
 
+  initFutureKulkas(): void {
+    this.futureKulkas = new Array<KulkaColors>();
+    for (let index = 0; index < Resources.NUMBER_OF_PER_TOUR; index++) {
+      this.futureKulkas.push(this.getRandomKulkaColor());
+    }
+  }
   processClick(boardElement: BoardElement): void {
-
+    this.displayListOfFreePlaces();
     if (this.listOfFreePlaces.length === 0) {
-      console.log('!!!!GAME OVER!!!!!');
+      this.isGameOver = true;
       return;
     }
     this.isClicked = !this.isClicked;
@@ -41,33 +51,176 @@ export class BoardComponent implements OnInit {
         return;
       }
       this.clickedKulka = boardElement;
-      console.log('Clicked Kulka');
-      console.log(this.clickedKulka);
       return;
     }
-
-    console.log('Board Element');
-    console.log(boardElement);
 
     if ( this.clickedKulka.position.x === boardElement.position.x
       && this.clickedKulka.position.y === boardElement.position.y) {
-        console.log('You\'ve clicked on the same position');
+        alert('You\'ve clicked on the same position');
       return;
     }
-
+    if (boardElement.isOccupied) {
+      alert('you clicked on the ball');
+      return;
+    }
     this.changeKulkaPosition(boardElement.position);
-    this.addRandomKulkasOnBoard(Resources.NUMBER_OF_PER_TOUR);
+    if (!this.checkMatch(boardElement.position)) {
+      this.addRandomKulkasOnBoard(Resources.NUMBER_OF_PER_TOUR);
+      this.checkMatch(boardElement.position);
+    }
+    this.displayListOfFreePlaces();
+    this.clickedKulka = this.board[boardElement.position.y][boardElement.position.x];
 
+  }
+  getFutureKulkas(): void {
+    for (let i = 0; i < this.futureKulkas.length; i++) {
+      this.futureKulkas[i] = this.getRandomKulkaColor();
+    }
+  }
+  checkMatch(position: Position): boolean {
+    const positionsToRemove = new Array<Position>();
+    let tempPositions = new Array<Position>();
+
+    tempPositions = this.checkHorizontal(position);
+    tempPositions.forEach( e => positionsToRemove.push(new Position(e.x, e.y)));
+
+    tempPositions = this.checkVertical(position);
+    tempPositions.forEach( e => positionsToRemove.push(new Position(e.x, e.y)));
+
+    tempPositions = this.checkDiagonal1(position);
+    tempPositions.forEach( e => positionsToRemove.push(new Position(e.x, e.y)));
+
+    tempPositions = this.checkDiagonal2(position);
+    tempPositions.forEach( e => positionsToRemove.push(new Position(e.x, e.y)));
+
+    positionsToRemove.forEach(element => {
+      this.removeKulkaFromPosition(element.x, element.y);
+    });
+    return positionsToRemove.length > 0;
+  }
+
+  checkHorizontal(position: Position): Array<Position> {
+
+    let tempPositions = new Array<Position>();
+    let counter = 0;
+    for ( let i = 0 ; i < 9; i++ ) {
+      if (this.board[i][position.x].kulkaColor === this.board[position.y][position.x].kulkaColor ) {
+        counter++;
+        tempPositions.push(new Position(position.x, i));
+      } else  if (counter < 5 ) {
+        tempPositions = [];
+        counter = 0;
+      } else {
+        break;
+      }
+    }
+
+    if ( tempPositions.length < 5) {
+      tempPositions = [];
+    }
+
+    return tempPositions;
+  }
+  checkVertical(position: Position): Array<Position> {
+    let tempPositions = new Array<Position>();
+    let counter = 0;
+    for ( let i = 0 ; i < 9; i++ ) {
+      if (this.board[position.y][i].kulkaColor === this.board[position.y][position.x].kulkaColor ) {
+        counter++;
+        tempPositions.push(new Position(i, position.y));
+      } else  if (counter < 5 ) {
+        tempPositions = [];
+        counter = 0;
+      } else {
+        break;
+      }
+    }
+
+    if ( tempPositions.length < 5) {
+      tempPositions = [];
+    }
+    return tempPositions;
+  }
+  checkDiagonal1(position: Position): Array<Position> {
+    let startx = 0;
+    let starty = 0;
+    if (position.x < position.y) {
+      startx = 0;
+      starty = position.y - position.x;
+    } else {
+      starty = 0;
+      startx = position.x - position.y;
+    }
+    let tempPositions = new Array<Position>();
+    let counter = 0;
+    for ( let i = 0 ; i < 9; i++ ) {
+      if ( starty + i > 8 || startx + i > 8) {
+        break;
+      }
+      if (this.board[starty + i][startx + i].kulkaColor === this.board[position.y][position.x].kulkaColor ) {
+        counter++;
+        tempPositions.push(new Position(startx + i, starty + i));
+      } else  if (counter < 5 ) {
+        tempPositions = [];
+        counter = 0;
+      } else {
+        break;
+      }
+    }
+
+    if ( tempPositions.length < 5) {
+      tempPositions = [];
+    }
+
+    return tempPositions;
+  }
+  checkDiagonal2(position: Position): Array<Position> {
+    let startx = 0;
+    let starty = 0;
+    if (position.x > position.y) {
+      starty = position.x + position.y > 8 ? 8 : position.x + position.y;
+      startx = position.x + position.y > 8 ? (position.x + position.y) - 8 : 0;
+    } else {
+      starty = position.y + position.x > 8 ? 8 : position.y + position.x;
+      startx = position.x + position.y > 8 ? (position.x + position.y) - 8 : 0;
+    }
+    let tempPositions = new Array<Position>();
+    let counter = 0;
+    for ( let i = 0 ; i < 9; i++ ) {
+      if ( starty - i < 0 || startx + i > 8) {
+        break;
+      }
+      if (this.board[starty - i][startx + i].kulkaColor === this.board[position.y][position.x].kulkaColor ) {
+        counter++;
+        tempPositions.push(new Position(startx + i, starty - i));
+      } else  if (counter < 5 ) {
+        tempPositions = [];
+        counter = 0;
+      } else {
+        break;
+      }
+    }
+
+    if ( tempPositions.length < 5) {
+      tempPositions = [];
+    }
+
+    return tempPositions;
   }
   removeKulkaFromPosition(x: number, y: number) {
     this.board[y][x].isOccupied = false;
     this.board[y][x].kulkaColor = KulkaColors.NoColor;
-    this.listOfFreePlaces.push(new Position(this.clickedKulka.position.x, this.clickedKulka.position.y));
-    this.listOfFreePlaces.filter( e => e.y !== y && e.x !== x);
+    this.listOfFreePlaces.push(new Position(x, y));
   }
   addKulkaOnPosition(destination: Position) {
     this.board[destination.y][destination.x].isOccupied = true;
     this.board[destination.y][destination.x].kulkaColor = this.clickedKulka.kulkaColor;
+    for (let i = 0; i < this.listOfFreePlaces.length; i++) {
+      if (this.listOfFreePlaces[i].y === destination.y && this.listOfFreePlaces[i].x === destination.x) {
+        this.listOfFreePlaces.splice(i, 1);
+        break;
+      }
+    }
   }
   changeKulkaPosition(destination: Position) {
     this.addKulkaOnPosition(destination);
@@ -84,12 +237,13 @@ export class BoardComponent implements OnInit {
       const randomPlace = Math.floor(Math.random() * this.listOfFreePlaces.length);
       const temp = this.listOfFreePlaces[randomPlace];
       this.board[temp.y][temp.x].isOccupied = true;
-      this.board[temp.y][temp.x].kulkaColor = this.getRandomKulkaColor();
+      this.board[temp.y][temp.x].kulkaColor = this.futureKulkas[i];
       this.listOfFreePlaces.splice(randomPlace, 1);
-      console.log(this.listOfFreePlaces.length);
+      // console.log(this.listOfFreePlaces.length);
 
     }
-    console.log(this.listOfFreePlaces);
+    this.getFutureKulkas();
+    // console.log(this.listOfFreePlaces);
   }
 
   getRandomKulkaColor(): KulkaColors {
@@ -107,5 +261,18 @@ export class BoardComponent implements OnInit {
   ngOnInit() {
 
   }
+  displayListOfFreePlaces(): void {
+    let testBoard: any[] | boolean[][][][];
+    testBoard = [];
+    for (let i = 0; i < Resources.NUMBER_OF_ROWS; i++) {
+      testBoard[i] = [];
+      for (let j = 0; j < Resources.NUMBER_OF_ROWS; j++) {
+        testBoard[i][j] = false;
+      }
+    }
+    this.listOfFreePlaces.forEach(e => testBoard[e.y][e.x] = true);
+    console.table(testBoard);
+  }
+
 
 }
