d8ef01bcb48ca76f5fe3def89e42d540230eedfa
Pawel Tarsala (PLT) <PLT@kmd.dk>
2019-01-05 02:47:25 +0100

    fix language problem

--- Kulki/src/app/app.component.ts
+++ Kulki/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Resources } from './resources';
-import { User } from './Models/user';
-import { HttpClient } from '@angular/common/http';
 import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
@@ -11,11 +9,21 @@ import { HttpService } from './http.service';
 })
 export class AppComponent {
   constructor(private translate: TranslateService, private httpService: HttpService) {
+    this.loadSetting();
     translate.setDefaultLang(Resources.LANGUAGE);
     httpService.setGuest();
   }
 
   switchLanguage(language: string) {
     this.translate.use(language);
+  }
+  loadSetting() {
+    this.httpService.getSettings().subscribe( (a: any) => {
+      Resources.LANGUAGE = a.language;
+      Resources.THEME = a.theme;
+    }, err => console.log(err),
+    () => {
+      this.switchLanguage(Resources.LANGUAGE);
+    });
   }
 }
